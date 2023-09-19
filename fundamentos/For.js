6155c35aaba37939d00a387155863ba237684ede
Andres Cristian <cristian.yapiticona@gmail.com>
2023-09-18 22:10:56 -0300

    Add files via upload

---
+++ fundamentos/For.js
@@ -0,0 +1,15 @@
+/**
+ * Estudo do Laço "for"
+ */
+
+console.clear()
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+console.log("Fim do Laço for")
+console.log("________________")
+
+for (let j = 10; j > 0; j--) {
+    console.log(j)
+}
+console.log("Fim da Ignição")
